3a81fb73bfbf182cd0610ee6d17bd35ec046d689
casey.Q.Wood <caseywood252@gmail.com>
2021-06-15 21:27:16 -0700

    time ago working minimally

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,12 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+// timeago.format(1623707875652);
+
+$(document).ready(function() {
+  const timeElement = $('.time')
+  const time = timeElement.data('time')
+  const timeAgo = timeago.format(time)
+  timeElement.text(timeAgo)
+
+});
