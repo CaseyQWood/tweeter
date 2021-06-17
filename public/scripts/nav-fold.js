cf91575da051970fd7d066996006e71c26d062e3
casey.Q.Wood <caseywood252@gmail.com>
2021-06-16 23:10:12 -0700

    folding search bar

---
+++ public/scripts/nav-fold.js
@@ -0,0 +1,9 @@
+$('documents').ready(function() {
+  
+  $('.quick-link').click(function() {
+    
+    $('.new-tweet').first().is(':hidden') ? $('.new-tweet').slideDown(): $('.new-tweet').slideUp()
+
+  })
+
+})
